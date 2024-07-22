ca8019d0770d39d5e54995b9eb875adf3df46a61
NoeGP <100245796+NoeGP@users.noreply.github.com>
2024-07-22 00:23:22 -0500

    Update getLocations.php

--- ExamenP3/getLocations.php
+++ ExamenP3/getLocations.php
@@ -1,38 +1,28 @@
 <?php
-header('Content-Type: application/json');
 $servername = "ba0pecttwocifsyryd4m-mysql.services.clever-cloud.com";
 $username = "uvlenx1awvdur1wk";
 $password = "dsh4rUBseIkNcEqO16oC";
 $dbname = "ba0pecttwocifsyryd4m";
 
-// Crear conexión
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Verificar conexión
 if ($conn->connect_error) {
-    echo "<html><h1>Conexión fallida: " . $conn->connect_error . "</h1></html>";
-} else {
-    echo "<html><h1>Conexión exitosa</h1></html>";
+    die("Conexión fallida: " . $conn->connect_error);
 }
-$sql = "SELECT * FROM Lugares";
+
+$sql = "SELECT `Biblioteca`,`BloqueA`,`BloqueB`,`Laboratorio` FROM Lugares;";
 $result = $conn->query($sql);
 
 $locations = array();
 
 if ($result->num_rows > 0) {
-    // Salida de cada fila
     while($row = $result->fetch_assoc()) {
         $locations[] = $row;
     }
-} else {
-    echo "0 results";
 }
-
-
 
 $conn->close();
 
+header('Content-Type: application/json');
 echo json_encode($locations);
-
 ?>
-
